9de0bc7268415e7a17cb5675d8944b355199b604
Konstantin Agafonov <konstantin.agafonov@gmail.com>
2017-04-29 14:04:34 +0300

    get parameters - version 2

--- lot.php
+++ lot.php
@@ -28,9 +28,11 @@ if (isset($_GET['id']) and ($_GET['id']!='')) {
     $lot_id = (int) $_GET['id'];
     if (!isset($lots[$lot_id])) {
         header('HTTP/1.1 404 Not Found');
+        die('Запрошенного лота не существует!');
     }
 } else {
     header('HTTP/1.1 404 Not Found');
+    die('Запрошенного лота не существует!');
 }
 
 ?>
@@ -42,8 +44,8 @@ if (isset($_GET['id']) and ($_GET['id']!='')) {
 
 <?=includeTemplate('templates/lots.php',[
     'bets' => $bets,
-    'lot' => ($lot_id) ? $lots[$lot_id] : null,
-    'category' => ($lot_id) ? $categories[$lots[$lot_id]['category']] : null
+    'lot' => $lots[$lot_id],
+    'category' => $categories[$lots[$lot_id]['category']]
 ]);?>
 
 <?=includeTemplate('templates/footer.php');?>

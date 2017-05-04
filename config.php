995f524b83663cbaa1725fbbfd4c4b666278917f
Konstantin Agafonov <konstantin.agafonov@gmail.com>
2017-05-04 12:44:28 +0300

    task13 - version 2

--- config.php
+++ config.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+error_reporting(E_ALL);
+
 // устанавливаем часовой пояс в Московское время
 date_default_timezone_set('Europe/Moscow');
 
--- templates/main.php
+++ templates/main.php
@@ -41,7 +41,7 @@
                         <img src="<?=htmlspecialchars($lot['pic']);?>" width="350" height="260" alt="<?=htmlspecialchars($lot['name']);?>">
                     </div>
                     <div class="lot__info">
-                        <span class="lot__category"><?=htmlspecialchars($categories[$lot['category']]);?></span>
+                        <span class="lot__category"><?=htmlspecialchars($data['categories'][$lot['category']]);?></span>
                         <h3 class="lot__title"><a class="text-link" href="lot.php?id=<?=$lot_id;?>"><?=$lot['name'];?></a></h3>
                         <div class="lot__state">
                             <div class="lot__rate">

8124f47ba030e1da1428373db10220014b41eb0f
Konstantin Agafonov <konstantin.agafonov@gmail.com>
2017-05-14 16:34:19 +0300

    task21

--- functions.php
+++ functions.php
@@ -22,3 +22,74 @@ function relativeTime(int $time): string
     }
     return round($diff / (60 * 60)) . " часов назад";
 }
+
+require_once 'mysql_helper.php';
+
+function db_select(
+                object $db_conn,
+                string $sql,
+                array $data = []
+            ) : array {
+    $prepared_sql = db_get_prepare_stmt($db_conn,$sql,$data);
+    $result = mysqli_stmt_execute($prepared_sql);
+    if (!$result) {
+        $error = mysqli_error($db_conn);
+        die("Ошибка MySQL: " . $error);
+    }
+    return mysqli_fetch_all($result,MYSQLI_ASSOC);
+}
+
+function db_insert(
+                object $db_conn,
+                string $sql,
+                array $data = []
+            ) : array {
+    $prepared_sql = db_get_prepare_stmt($db_conn,$sql,$data);
+    $result = mysqli_stmt_execute($prepared_sql);
+    if (!$result) {
+        $error = mysqli_error($db_conn);
+        die("Ошибка MySQL: " . $error);
+    }
+    $records_count = mysqli_num_rows($result);
+    return $records_count;
+}
+
+function db_update(
+                object $db_conn,
+                string $table_name,
+                array $data,
+                array $conditions
+            ) : array {
+
+    $field_names = [];
+    $field_val_phldr = [];
+    foreach ($data as $key => $value) {
+        $field_names[] = $key;
+        $field_val_phldr[] = '?';
+    }
+
+    $where_clause = '';
+    if (isset($conditions)) {
+        $where_clause = ' where ';
+        $where_conditions = [];
+        foreach ($conditions as $name => $value) {
+            $where_conditions[] = " " . mysqli_real_escape_string($name) . " = ? ";
+        }
+        $where_clause .= explode(' and ',$where_conditions);
+    }
+
+    $update_sql = 'update ' . mysqli_real_escape_string($table_name)
+        . ' (' . explode(',',$field_names) . ') values ('
+        . explode(',',$field_val_phldr) . ') '
+        . $where_clause . ';';
+
+    $prepared_sql = db_get_prepare_stmt($db_conn,$update_sql,array_merge(array_values($data),array_values($conditions)));
+    $result = mysqli_stmt_execute($prepared_sql);
+    if (!$result) {
+        $error = mysqli_error($db_conn);
+        die("Ошибка MySQL: " . $error);
+    }
+    $records_count = mysqli_num_rows($result);
+    return $records_count;
+}
+
